6ab35b62452c1a8d3daec0ce8c741474307945b2
DINHHUAN <huanden246@gmail.com>
2025-10-08 17:19:25 +0700

    Sample code day 2

---
+++ Day02/Day02/DataNews.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02
+{
+    internal class DataNews : EventArgs
+    {
+        private object _data;
+        public DataNews(object data)
+        {
+            _data = data;
+        }
+        public object GetData()
+        {
+            return _data;
+        }
+        public void SetData(string data)
+        {
+            _data = data;
+        }
+       
+    }
+}
---
+++ Day02/Day02/Publisher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02
+{
+    internal class Publisher
+    {
+        public event EventHandler evt;
+        public void Send(object data)
+        {
+            evt?.Invoke(this, new DataNews(data));
+        }
+    }
+}
